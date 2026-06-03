2d11fa9c7803e3dd2ff89ff5020052c1478e6df4
Kanasani Dhanush <Kanasanidhanush7@gmail.com>
2026-06-03 12:23:06 +0530

    fixed shape mismatch error in PredictAction

--- FraudApp/views.py
+++ FraudApp/views.py
@@ -19,9 +19,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 from imblearn.over_sampling import SMOTE
 from sklearn.naive_bayes import GaussianNB
 
@@ -33,12 +31,12 @@ precision = []
 recall = [] 
 fscore = []
 
-#function to calculate all metrics
+# Function to calculate all metrics
 def calculateMetrics(algorithm, y_test, predict):
-    a = (accuracy_score(y_test,predict)*100)
-    p = (precision_score(y_test, predict,average='macro') * 100)
-    r = (recall_score(y_test, predict,average='macro') * 100)
-    f = (f1_score(y_test, predict,average='macro') * 100)
+    a = (accuracy_score(y_test, predict) * 100)
+    p = (precision_score(y_test, predict, average='macro') * 100)
+    r = (recall_score(y_test, predict, average='macro') * 100)
+    f = (f1_score(y_test, predict, average='macro') * 100)
     a = round(a, 3)
     p = round(p, 3)
     r = round(r, 3)
@@ -49,21 +47,22 @@ def calculateMetrics(algorithm, y_test, predict):
     fscore.append(f)
     return algorithm
 
+# Training Model Pipeline Initialization
 dataset = pd.read_csv("Dataset/PS_20174392719_1491204439457_log.csv")
 Y = dataset['isFraud'].ravel()
 unique, count = np.unique(Y, return_counts=True)
-dataset.drop(['step', 'type', 'isFraud', 'isFlaggedFraud'], axis = 1,inplace=True)
+dataset.drop(['step', 'type', 'isFraud', 'isFlaggedFraud'], axis=1, inplace=True)
 
 label_encoder = []
 columns = dataset.columns
 types = dataset.dtypes.values
 for j in range(len(types)):
     name = types[j]
-    if name == 'object': #finding column with object type
+    if name == 'object': # finding column with object type
         le = LabelEncoder()
-        dataset[columns[j]] = pd.Series(le.fit_transform(dataset[columns[j]].astype(str)))#encode all str columns to numeric
+        dataset[columns[j]] = pd.Series(le.fit_transform(dataset[columns[j]].astype(str))) # encode all str columns to numeric
         label_encoder.append([columns[j], le])
-dataset.fillna(dataset.mean(), inplace = True)
+dataset.fillna(dataset.mean(), inplace=True)
 
 X = dataset.values
 
@@ -74,6 +73,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 data = np.load("model/data.npy", allow_pickle=True)
 X_train, X_test, y_train, y_test = data
 train_size = X_train.shape[0]
+
 rf = RandomForestClassifier(n_estimators=1)
 rf.fit(X_train, y_train)
 predict = rf.predict(X_test)
@@ -98,80 +98,89 @@ nb.fit(X_train, y_train)
 predict = nb.predict(X_test)
 calculateMetrics("nb", y_test, predict)
 
+
 def Predict(request):
     if request.method == 'GET':
         return render(request, 'Predict.html', {})
+
 
 def PredictAction(request):
     if request.method == 'POST':
         global rf, scaler, labels, dataset
         myfile = request.FILES['t1'].read()
         filename = request.FILES['t1'].name
-        if os.path.exists('FraudApp/static/'+filename):
-            os.remove('FraudApp/static/'+filename)
-        with open('FraudApp/static/'+filename, "wb") as file:
+        if os.path.exists('FraudApp/static/' + filename):
+            os.remove('FraudApp/static/' + filename)
+        with open('FraudApp/static/' + filename, "wb") as file:
             file.write(myfile)
         file.close()
-        testData = pd.read_csv('FraudApp/static/'+filename)
+        
+        testData = pd.read_csv('FraudApp/static/' + filename)
         data = testData.values
-        testData.drop(['step', 'type'], axis = 1,inplace=True)
+        
+        # FIXED: Dropping evaluating features ('isFraud', 'isFlaggedFraud') so exactly 7 columns match the training scaler
+        testData.drop(['step', 'type', 'isFraud', 'isFlaggedFraud'], axis=1, inplace=True, errors='ignore')
+        
         for i in range(len(label_encoder)):
             le = label_encoder[i]
-            testData[le[0]] = pd.Series(le[1].transform(testData[le[0]].astype(str)))#encode all str columns to numeric
-        testData.fillna(dataset.mean(), inplace = True)
+            testData[le[0]] = pd.Series(le[1].transform(testData[le[0]].astype(str))) # encode all str columns to numeric
+        testData.fillna(dataset.mean(), inplace=True)
         testData = scaler.transform(testData)
         predict = rf.predict(testData)
-        output='<table border=1 align=center width=100%><tr><th><font size="3" color="black">Test Data</th><th><font size="3" color="black">Detection Status</th></tr>'
+        
+        output = '<table border=1 align=center width=100%><tr><th><font size="3" color="black">Test Data</th><th><font size="3" color="black">Detection Status</th></tr>'
         for i in range(len(predict)):
             if predict[i] == 0:
-                output += '<tr><td><font size="3" color="black">'+str(data[i])+'</td><td><font size="4" color="green">Normal Transaction</td></tr>'
+                output += '<tr><td><font size="3" color="black">' + str(data[i]) + '</td><td><font size="4" color="green">Normal Transaction</td></tr>'
             else:
-                output += '<tr><td><font size="3" color="black">'+str(data[i])+'</td><td><font size="4" color="red">Fraud Transaction</td></tr>'
-        output+= "</table></br></br></br></br>"       
-        context= {'data':output}
+                output += '<tr><td><font size="3" color="black">' + str(data[i]) + '</td><td><font size="4" color="red">Fraud Transaction</td></tr>'
+        output += "</table></br></br></br></br>"       
+        context = {'data': output}
         return render(request, 'UserScreen.html', context)
+
 
 def TrainML(request):
     if request.method == 'GET':
         global X_train, X_test, y_train, y_test, labels
         global accuracy, precision, recall, fscore, conf_matrix
-        output='<table border=1 align=center width=100%><tr><th><font size="3" color="black">Algorithm Name</th><th><font size="3" color="black">Accuracy</th>'
+        output = '<table border=1 align=center width=100%><tr><th><font size="3" color="black">Algorithm Name</th><th><font size="3" color="black">Accuracy</th>'
         output += '<th><font size="3" color="black">Precision</th><th><font size="3" color="black">Recall</th><th><font size="3" color="black">FSCORE</th></tr>'
         algorithms = ['Random Forest', 'Naive Bayes', 'Random Forest with Smote', 'Naive Bayes with Smote']
         for i in range(len(algorithms)):
-            output += '<tr><td><font size="3" color="black">'+algorithms[i]+'</td><td><font size="3" color="black">'+str(accuracy[i])+'</td><td><font size="3" color="black">'+str(precision[i])+'</td>'
-            output += '<td><font size="3" color="black">'+str(recall[i])+'</td><td><font size="3" color="black">'+str(fscore[i])+'</td></tr>'
-        output+= "</table></br>"
-        figure, axis = plt.subplots(nrows=1, ncols=2,figsize=(10, 3))#display original and predicted segmented image
+            output += '<tr><td><font size="3" color="black">' + algorithms[i] + '</td><td><font size="3" color="black">' + str(accuracy[i]) + '</td><td><font size="3" color="black">' + str(precision[i]) + '</td>'
+            output += '<td><font size="3" color="black">' + str(recall[i]) + '</td><td><font size="3" color="black">' + str(fscore[i]) + '</td></tr>'
+        output += "</table></br>"
+        figure, axis = plt.subplots(nrows=1, ncols=2, figsize=(10, 3)) # display original and predicted segmented image
         axis[0].set_title("Confusion Matrix Prediction Graph")
         axis[1].set_title("All Algorithms Comparison Graph")
-        ax = sns.heatmap(conf_matrix, xticklabels = labels, yticklabels = labels, annot = True, cmap="viridis" ,fmt ="g", ax=axis[0]);
-        ax.set_ylim([0,len(labels)])
-        df = pd.DataFrame([['Random Forest','Accuracy',accuracy[0]],['Random Forest','Precision',precision[0]],['Random Forest','Recall',recall[0]],['Random Forest','FSCORE',fscore[0]],
-                           ['Naive Bayes','Accuracy',accuracy[1]],['Naive Bayes','Precision',precision[1]],['Naive Bayes','Recall',recall[1]],['Naive Bayes','FSCORE',fscore[1]],
-                           ['Random Forest with Smote','Accuracy',accuracy[2]],['Random Forest with Smote','Precision',precision[2]],['Random Forest with Smote','Recall',recall[2]],['Random Forest with Smote','FSCORE',fscore[2]],
-                           ['Naive Bayes with Smote','Accuracy',accuracy[3]],['Naive Bayes with Smote','Precision',precision[3]],['Naive Bayes with Smote','Recall',recall[3]],['Naive Bayes with Smote','FSCORE',fscore[3]],
-                          ],columns=['Parameters','Algorithms','Value'])
+        ax = sns.heatmap(conf_matrix, xticklabels=labels, yticklabels=labels, annot=True, cmap="viridis", fmt="g", ax=axis[0])
+        ax.set_ylim([0, len(labels)])
+        df = pd.DataFrame([['Random Forest', 'Accuracy', accuracy[0]], ['Random Forest', 'Precision', precision[0]], ['Random Forest', 'Recall', recall[0]], ['Random Forest', 'FSCORE', fscore[0]],
+                           ['Naive Bayes', 'Accuracy', accuracy[1]], ['Naive Bayes', 'Precision', precision[1]], ['Naive Bayes', 'Recall', recall[1]], ['Naive Bayes', 'FSCORE', fscore[1]],
+                           ['Random Forest with Smote', 'Accuracy', accuracy[2]], ['Random Forest with Smote', 'Precision', precision[2]], ['Random Forest with Smote', 'Recall', recall[2]], ['Random Forest with Smote', 'FSCORE', fscore[2]],
+                           ['Naive Bayes with Smote', 'Accuracy', accuracy[3]], ['Naive Bayes with Smote', 'Precision', precision[3]], ['Naive Bayes with Smote', 'Recall', recall[3]], ['Naive Bayes with Smote', 'FSCORE', fscore[3]],
+                          ], columns=['Parameters', 'Algorithms', 'Value'])
         df.pivot("Parameters", "Algorithms", "Value").plot(kind='bar', ax=axis[1])        
         buf = io.BytesIO()
         plt.savefig(buf, format='png', bbox_inches='tight')
         img_b64 = base64.b64encode(buf.getvalue()).decode()
         plt.clf()
         plt.cla()
-        context= {'data':output, 'img': img_b64}
+        context = {'data': output, 'img': img_b64}
         return render(request, 'UserScreen.html', context)
+
 
 def LoadDataset(request):    
     if request.method == 'GET':
         global unique, count, labels, dataset
         output = '<font size="3" color="black">Online Fraud Payment Detection Dataset Loaded</font><br/>'
-        output += '<font size="3" color="blue">Total records found in Dataset = '+str(dataset.shape[0])+'</font><br/>'
-        output += '<font size="3" color="blue">Different Class Labels found in Dataset = '+str(labels)+'</font><br/><br/>'
-        #visualizing class labels count found in dataset
+        output += '<font size="3" color="blue">Total records found in Dataset = ' + str(dataset.shape[0]) + '</font><br/>'
+        output += '<font size="3" color="blue">Different Class Labels found in Dataset = ' + str(labels) + '</font><br/><br/>'
+        # visualizing class labels count found in dataset
         height = count
         bars = labels
         y_pos = np.arange(len(bars))
-        plt.figure(figsize = (4, 3)) 
+        plt.figure(figsize=(4, 3)) 
         plt.bar(y_pos, height)
         plt.xticks(y_pos, bars)
         plt.xlabel("Imbalanced Dataset Class Label Graph")
@@ -181,20 +190,21 @@ def LoadDataset(request):
         img_b64 = base64.b64encode(buf.getvalue()).decode()
         plt.clf()
         plt.cla()
-        context= {'data':output, 'img': img_b64}
+        context = {'data': output, 'img': img_b64}
         return render(request, 'UserScreen.html', context)
+
 
 def BalancedData(request):    
     if request.method == 'GET':
         global X_train, X_test, y_train, y_test, X, Y, train_size
         output = '<font size="3" color="black">Smote Balancing Dataset Details</font><br/>'
-        output += '<font size="3" color="blue">Training Size Before Applying SMOTE = '+str(train_size)+'</font><br/>'
-        output += '<font size="3" color="blue">Training Size after Applying SMOTE = '+str(X_train.shape[0])+'</font><br/><br/>'
-        unique, count = np.unique(y_train)
+        output += '<font size="3" color="blue">Training Size Before Applying SMOTE = ' + str(train_size) + '</font><br/>'
+        output += '<font size="3" color="blue">Training Size after Applying SMOTE = ' + str(X_train.shape[0]) + '</font><br/><br/>'
+        unique, count = np.unique(y_train, return_counts=True)
         height = count
         bars = labels
         y_pos = np.arange(len(bars))
-        plt.figure(figsize = (4, 3)) 
+        plt.figure(figsize=(4, 3)) 
         plt.bar(y_pos, height)
         plt.xticks(y_pos, bars)
         plt.xlabel("Balanced Dataset After Applying SMOTE Graph")
@@ -204,22 +214,21 @@ def BalancedData(request):
         img_b64 = base64.b64encode(buf.getvalue()).decode()
         plt.clf()
         plt.cla()
-        context= {'data':output, 'img': img_b64}
+        context = {'data': output, 'img': img_b64}
         return render(request, 'UserScreen.html', context)    
 
-from .models import Register
 
 def UserLoginAction(request):
     if request.method == 'POST':
         username = request.POST.get('t1').strip()
         password = request.POST.get('t2').strip()
 
-        print("DEBUG LOGIN:", username, password)  # 👈 add this
+        print("DEBUG LOGIN:", username, password)
 
         user = Register.objects.filter(username=username).first()
 
         if user:
-            print("DB PASSWORD:", user.password)  # 👈 debug
+            print("DB PASSWORD:", user.password)
             if user.password == password:
                 return render(request, "UserScreen.html", {'data': 'Welcome ' + username})
 
